41054fd2576d17764190b115ad2c2848afe86f5f
Denisa Iacob <denisaiacob18@gmail.com>
2017-02-08 11:31:25 -0500

    added fields and task model

---
+++ ToDoListExample/Models/task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ToDoListExample.Models
+{
+    public class task
+    {
+        [Key]
+        public int TaskID { get; set; }
+        public string TaskName { get; set; }
+        public string Description { get; set; }
+        public bool IsDone { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
