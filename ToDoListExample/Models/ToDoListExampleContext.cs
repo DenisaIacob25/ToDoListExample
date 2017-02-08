7457af6934fbcadef6d1b8b39142160b3d959aba
Denisa Iacob <denisaiacob18@gmail.com>
2017-02-08 13:24:12 -0500

    added controllers

---
+++ ToDoListExample/Models/ToDoListExampleContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ToDoListExample.Models
+{
+    public class ToDoListExampleContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public ToDoListExampleContext() : base("name=ToDoListExampleContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<ToDoListExample.Models.task> tasks { get; set; }
+
+        public System.Data.Entity.DbSet<ToDoListExample.Models.List> Lists { get; set; }
+    }
+}
