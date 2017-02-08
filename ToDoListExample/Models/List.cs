fcb8f90c4b30402d5aa5b05cc5fb9286366bad91
Denisa Iacob <denisaiacob18@gmail.com>
2017-02-08 11:40:34 -0500

    created list model and key within

---
+++ ToDoListExample/Models/List.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ToDoListExample.Models
+{
+    public class List
+    {
+        [Key]
+        public int ListID { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
